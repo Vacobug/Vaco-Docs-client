34180f174787bc676974aad2a7005e47151e54c5
Takhyun Kim <kimkih1218@naver.com>
2021-08-30 22:26:46 +0900

    Initial setting

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    https: true,
+    historyApiFallback: true,
+    open: false,
+    compress: false,
+    hot: true,
+    port: 8080,
+    host: 'localhost',
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    watchFiles: [ '../templates/**/*.*', '../src/**/*.*' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
