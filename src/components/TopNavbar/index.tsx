f1ef5b9eb63799760dd82497dbd267a3ab789ace
Takhyun Kim <kimkih1218@gmail.com>
2021-09-02 01:15:18 +0900

    feat: Add TopNavber component

---
+++ src/components/TopNavbar/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState, ChangeEvent } from "react";
+import styled from "styled-components";
+import { AiOutlineSearch } from "react-icons/ai";
+
+import TextInputWithIcon from "../TextInput/TextInputWithIcon";
+
+const StyledNavbar = styled.article`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 1em;
+`;
+
+function TopNavbar() {
+  const [text, setText] = useState<string>("");
+
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <StyledNavbar>
+      <TextInputWithIcon
+        text={text}
+        placeholder="검색"
+        handleTextChange={handleTextChange}
+      >
+        <AiOutlineSearch size={30} />
+      </TextInputWithIcon>
+    </StyledNavbar>
+  );
+}
+
+export default TopNavbar;
