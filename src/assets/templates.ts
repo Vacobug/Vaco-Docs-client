b3e749a36c95e2b73059e636de29e68d4a4b5b61
Takhyun Kim <kimkih1218@gmail.com>
2021-09-09 00:49:58 +0900

    refactor: Add description key to templates array

--- src/assets/templates.ts
+++ src/assets/templates.ts
@@ -3,27 +3,33 @@ export type Documents = "none" | "report" | "resume" | "proposal" | "letter";
 export interface templateType {
   templateName: Documents;
   templateImageURL: string;
+  description: string;
 }
 
 export const templates = [
   {
     templateName: "none",
-    templateImageURL: "none_url"
+    templateImageURL: "none_url",
+    description: "내용 없음"
   },
   {
     templateName: "report",
-    templateImageURL: "report_url"
+    templateImageURL: "report_url",
+    description: "보고서"
   },
   {
     templateName: "resume",
-    templateImageURL: "resume_url"
+    templateImageURL: "resume_url",
+    description: "이력서"
   },
   {
     templateName: "proposal",
-    templateImageURL: "proposal_url"
+    templateImageURL: "proposal_url",
+    description: "서신"
   },
   {
     templateName: "letter",
-    templateImageURL: "letter_url"
+    templateImageURL: "letter_url",
+    description: "편지"
   },
 ];
